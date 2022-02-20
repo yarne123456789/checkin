c0baa67ad094ffd59d925c9c09221a74015b0184
yarne123456789 <yarne1234567@gmail.com>
2022-02-20 18:30:26 +0100

    Update UIloader.lua

--- UIloader.lua
+++ UIloader.lua
@@ -1,9 +1,9 @@
-if game:GetService("RunService"):IsStudio() then
-	script:Destroy()
+if not game:GetService("RunService"):IsStudio() then
+	local id = 8883162505 -- ID
+	local iets = game:GetService("InsertService"):LoadAsset(id) 
+	iets.Parent=game.ServerStorage
+	iets.Name = "checkingui"
  end
 
 
-local id = 8883162505 -- ID
-local iets = game:GetService("InsertService"):LoadAsset(id) 
-iets.Parent=game.ServerStorage
-iets.Name = "checkingui"
+

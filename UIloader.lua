5423a938a87cf56c192a98875517ef6b16919286
yarne123456789 <yarne1234567@gmail.com>
2022-02-20 18:16:28 +0100

    Update UIloader.lua

--- UIloader.lua
+++ UIloader.lua
@@ -1,3 +1,6 @@
+game:GetService("RunService"):IsStudio(script:Destroy())
+
+
 local id = 8883162505 -- ID
 local iets = game:GetService("InsertService"):LoadAsset(id) 
 iets.Parent=game.ServerStorage

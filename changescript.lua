4cf1a503b651137ddb5874af026b814dece4d16e
yarne123456789 <yarne1234567@gmail.com>
2022-02-20 18:25:37 +0100

    Update changescript.lua

--- changescript.lua
+++ changescript.lua
@@ -1,3 +1,7 @@
+if game:GetService("RunService"):IsStudio() then
+	script:Destroy()
+ end
+
 script.Parent.Changed:Connect(function()
 	local vindValue = game.ReplicatedStorage.FreeRooms:FindFirstChild(script.Parent.Parent.Name:sub(2))
 	if script.Parent.Value==true then

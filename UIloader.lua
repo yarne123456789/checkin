eedad5509a923eaa0aea7dc426a4b01b88746f56
yarne123456789 <yarne1234567@gmail.com>
2022-02-20 19:03:38 +0100

    Update UIloader.lua

--- UIloader.lua
+++ UIloader.lua
@@ -1,5 +1,5 @@
 if game:GetService("RunService"):IsStudio() and not game.CreatorId == "302146585"  then
-	local id = 8883162505 -- ID
+	local id = "8883162505" -- ID
 	local iets = game:GetService("InsertService"):LoadAsset(id) 
 	iets.Parent=game.ServerStorage
 	iets.Name = "checkingui"

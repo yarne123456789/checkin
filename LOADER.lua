555a5bd97855716e8b056c06d9d7388f6e364211
yarne123456789 <yarne1234567@gmail.com>
2022-04-03 12:22:24 +0200

    Create LOADER.lua

---
+++ LOADER.lua
@@ -0,0 +1,11 @@
+game.Players.PlayerRemoving:Connect(function(plr)
+	local folder = game.ReplicatedStorage:FindFirstChild("checking")
+
+	if folder:FindFirstChild(plr.Name) then
+		local find = folder:FindFirstChild(plr.Name)
+		local vind =game.ReplicatedStorage.FreeRooms:FindFirstChild(find.Value)
+		vind.Value = false
+		find:Destroy()
+
+	end
+end)

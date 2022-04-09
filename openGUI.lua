62aae2b88ab3fa71c68a7e6a44e515fe359fb3ee
yarne123456789 <yarne1234567@gmail.com>
2022-04-09 21:19:55 +0200

    Update openGUI.lua

--- openGUI.lua
+++ openGUI.lua
@@ -5,7 +5,7 @@ if game:GetService("RunService"):IsStudio() then
 script.Parent.ClickDetector.MouseClick:Connect(function(plr)
 	local speler = game.Players:FindFirstChild(plr.Name)
 	if not speler.PlayerGui:FindFirstChild("checkingui") then
-		local dupli = game.Workspace.checkingui.checkingui:Clone()
+		local dupli = game.ServerStorage.checkingui.checkingui:Clone()
 		dupli.Parent = speler.PlayerGui
 
 	end
